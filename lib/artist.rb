b13392ff714b8b23392f9a6a686014e9edb8a089
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2019-01-29 18:19:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,37 @@
 class Artist
+  attr_accessor :name 
+  attr_reader :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = [] #allows artist to have many songs 
+  end
+  
+  def add_song(song)
+    @songs.push(song)
+    song.artist = self
+  end
+  
+  def save 
+    @@all << self
+    self
+  end
+  
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+  
+  def self.find_or_create_by_name(searched_name)
+    self.all.find{|artist| artist.name == searched_name} || self.new(searched_name).save
+  end
+  
+  def self.all
+    @@all
+  end
+  
 
 
 end
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,21 @@
 class MP3Importer
+  attr_reader :path
+  
+  def initialize(kitten)
+    @path = kitten
+  end
+  
+  def files
+    Dir["#{@path}/*.mp3"].map do |filepath|
+      filepath.split("/").last 
+    end
+  end
+  
+  def import 
+    files.each do |some_filename|
+      Song.new_by_filename(some_filename)
+    end
+  end
   
 
 end
